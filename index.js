c4da7563f46d0e29cbf68f5bc73c321c167c3972
leMaik <an@maik.tk>
2016-11-09 01:06:30 +0100

    Remove node-binary to fix weird rare bugs.

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-const binary = require('binary')
+const util = require('util')
+const { Writable } = require('stream')
 const zlib = require('zlib')
 
+// width (int32bs), height (int32bs), spp (int32bs), renderTime (int64bs)
+const expectedHeaderCount = 4 + 4 + 4 + 8
+
+function DumpCheckStream (options) {
+  Writable.call(this, options)
+  this.headerCount = 0
+  this.header = []
+}
+util.inherits(DumpCheckStream, Writable)
+
+DumpCheckStream.prototype._write = function (chunk, enc, cb) {
+  if (this.headerCount < expectedHeaderCount) {
+    const headerPart = chunk.slice(0, Math.max(0, expectedHeaderCount - this.headerCount))
+    if (headerPart.length > 0) {
+      this.headerCount += headerPart.length
+      this.header.push(headerPart)
+    }
+  }
+
+  if (this.headerCount === expectedHeaderCount && this.width == null && this.height == null) {
+    const header = this.header[0].length >= 20 ? this.header[0] : Buffer.concat(this.header)
+    this.width = header.readInt32BE(0)
+    this.height = header.readInt32BE(4)
+    this.spp = header.readInt32BE(8)
+    this.renderTime = header.readIntBE(12, 8, true)
+    this.valid = true
+    this.emit('dump header')
+  }
+
+  cb()
+}
+
 const getDumpInfo = (dumpStream) => new Promise((resolve, reject) => {
-  let tapped = false
-  const ws = binary()
-    .word32bs('width')
-    .word32bs('height')
-    .word32bs('spp')
-    .word64bs('renderTime')
-    .tap((vars) => {
-      tapped = true
-      resolve(vars)
+  const ws = new DumpCheckStream()
+  ws.on('dump header', () => {
+    resolve({
+      width: ws.width,
+      height: ws.height,
+      spp: ws.spp,
+      renderTime: ws.renderTime
     })
+  })
   dumpStream.on('end', () => {
-    if (!tapped) {
-      reject(new Error('Unexpected end of dump stream'))
+    if (!ws.valid) {
+      reject(new Error('Invalid dump stream'))
     }
   })
   dumpStream.pipe(zlib.createGunzip()).pipe(ws)
