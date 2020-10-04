bced22a7834de962be8016107238e0f39b7be3f1
Maik Marschner <an@maik.tk>
2020-10-04 16:07:39 +0200

    Update the tests.

--- test/test.js
+++ test/test.js
@@ -1,55 +1,57 @@
 /* global describe, it */
 const chai = require('chai')
 const { expect } = chai
-const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
 const fs = require('fs')
 const path = require('path')
 const { getDumpInfo, getValidatedDumpInfo } = require('../')
 
 describe('getDumpInfo', () => {
-  it('should read information of valid render dumps', () => {
+  it('should read information of valid render dumps', async () => {
     const rs = fs.createReadStream(path.join(__dirname, 'valid.dump'))
-    return getDumpInfo(rs)
-      .then(({ width, height, spp, renderTime }) => {
-        expect(width).to.equal(123)
-        expect(height).to.equal(42)
-        expect(spp).to.equal(100)
-        expect(renderTime).to.equal(2792)
-      })
+    const { width, height, spp, renderTime } = await getDumpInfo(rs)
+    expect(width).to.equal(123)
+    expect(height).to.equal(42)
+    expect(spp).to.equal(100)
+    expect(renderTime).to.equal(2792n)
   })
 
-  it('should reject if the stream ends unexpectedly', () => {
+  it('should reject if the stream ends unexpectedly', async () => {
     const rs = fs.createReadStream(path.join(__dirname, 'invalid.dump'))
-    return expect(getDumpInfo(rs)).to.be.rejectedWith(Error)
+    try {
+      await getDumpInfo(rs)
+      expect.fail('expected to reject')
+    } catch (e) {
+      expect(e).to.be.instanceOf(Error)
+    }
   })
 
-  it('should not reject if the dump body is invalid', () => {
+  it('should not reject if the dump body is invalid', async () => {
     const rs = fs.createReadStream(path.join(__dirname, 'invalid-body.dump'))
-    return getDumpInfo(rs)
-      .then(({ width, height, spp, renderTime }) => {
-        expect(width).to.equal(123)
-        expect(height).to.equal(42)
-        expect(spp).to.equal(100)
-        expect(renderTime).to.equal(2792)
-      })
+    const { width, height, spp, renderTime } = await getDumpInfo(rs)
+    expect(width).to.equal(123)
+    expect(height).to.equal(42)
+    expect(spp).to.equal(100)
+    expect(renderTime).to.equal(2792n)
   })
 })
 
 describe('getValidatedDumpInfo', () => {
-  it('should read information of valid render dumps', () => {
+  it('should read information of valid render dumps', async () => {
     const rs = fs.createReadStream(path.join(__dirname, 'valid.dump'))
-    return getDumpInfo(rs)
-      .then(({ width, height, spp, renderTime }) => {
-        expect(width).to.equal(123)
-        expect(height).to.equal(42)
-        expect(spp).to.equal(100)
-        expect(renderTime).to.equal(2792)
-      })
+    const { width, height, spp, renderTime } = await getDumpInfo(rs)
+    expect(width).to.equal(123)
+    expect(height).to.equal(42)
+    expect(spp).to.equal(100)
+    expect(renderTime).to.equal(2792n)
   })
 
-  it('should reject if the header is valid but the dump body is not', () => {
+  it('should reject if the header is valid but the dump body is not', async () => {
     const rs = fs.createReadStream(path.join(__dirname, 'invalid-body.dump'))
-    return expect(getValidatedDumpInfo(rs)).to.be.rejectedWith(Error)
+    try {
+      await getValidatedDumpInfo(rs)
+      expect.fail('expected to reject')
+    } catch (e) {
+      expect(e).to.be.instanceOf(Error)
+    }
   })
 })
